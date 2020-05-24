8d7b1b1bf2bb7471f95cfb709f509a561fb9de7b
Valerie <valerie.flores64@gmail.com>
2020-05-23 21:25:42 -0700

    created initial files

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Readme_Generator
+# Awesome Project
+![GitHub custom badge](https://img.shields.io/badge/MyApp-isAwesome-blueviolet)
+
+## Table of Contents
+* [Installation](#installation)
+* [Tests](#tests)
+* [Usage](#usage)
+* [LICENSE](#license)
+* [Contributing](#contributing)
+* [Questions?](#questions?)
+
+## Installation
+In order to install this project, run `npm i` in the terminal
+
+## Tests
+To test this project, run `npm test′ in the terminal
+
+## Usage
+This is my awesome project.  You will be so impressed by its awesomeness
+
+## License
+This project uses the MIT license
+
+## Contributing
+Instructions for contributing:
+If you are awesome, you can contribute, too!
+
+## Questions?
+For questions, please contact the project owner, [valeriemiller5](mailto:valeriemiller5@hotmail.com).
---
+++ script.js
@@ -0,0 +1,129 @@
+const fs = require("fs");
+const inquire = require("inquirer");
+
+inquire
+    .prompt([
+        {
+            type: "input",
+            name: "githubName",
+            message: "Please entery your GitHub username:",
+            validate: function (input) {
+                const done = this.async();
+                if (input == "") {
+                    return done("You must enter a username")
+                } else {
+                    done(true)
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "inputEmail",
+            message: "Please enter your email address:",
+            validate: function (input) {
+                const done = this.async();
+                if (input == "") {
+                    return done("You must enter your email")
+                } else {
+                    done(true)
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "inputTitle",
+            message: "Please enter the title of your project:",
+            validate: function (input) {
+                const done = this.async();
+                if (input == "") {
+                    return done("You must enter a title")
+                } else {
+                    done(true)
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "projectDescription",
+            message: "Please give a brief description of your project:",
+            validate: function (input) {
+                const done = this.async();
+                if (input == "") {
+                    return done("You must enter a description")
+                } else {
+                    done(true)
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "install",
+            message: "How should the user install this project?",
+            default: "npm i"
+        },
+        {
+            type: "input",
+            name: "testing",
+            message: "How should the user test this code?",
+            default: "npm test"
+        },
+        {
+            type: "rawlist",
+            name: "licensing",
+            message: "Which license was used for this project?",
+            choices: ["MIT", "Apache License 2.0", "GNU", "Other", "No license was used"],
+        },
+        {
+            type: "input",
+            name: "contribution",
+            message: "How can others contribute to this project?",
+            validate: function (input) {
+                const done = this.async();
+                if (input == "") {
+                    return done("You must enter a description")
+                } else {
+                    done(true)
+                }
+            }
+        }
+    ])
+    .then(answers => {
+        console.log(answers)
+        const myShield = "![GitHub custom badge](https://img.shields.io/badge/MyApp-isAwesome-blueviolet)";
+        
+        // Due to template literal formatting, the spacing isn't "pretty" in the code, but renders well in the README
+        const response = `# ${answers.inputTitle}
+${myShield}
+
+## Table of Contents
+* [Installation](#installation)
+* [Tests](#tests)
+* [Usage](#usage)
+* [LICENSE](#license)
+* [Contributing](#contributing)
+* [Questions?](#questions?)
+
+## Installation
+In order to install this project, run \`${answers.install}\` in the terminal
+
+## Tests
+To test this project, run \`${answers.testing}′ in the terminal
+
+## Usage
+${answers.projectDescription}
+
+## License
+This project uses the ${answers.licensing} license
+
+## Contributing
+Instructions for contributing:
+${answers.contribution}
+
+## Questions?
+For questions, please contact the project owner, [${answers.githubName}](mailto:${answers.inputEmail}).
+`
+        fs.writeFile("README.md", response, err => {
+            if (err) throw err;
+            console.log("Success!");
+        })
+    });
